c3e52a31b4aa8e90e6e308f6385a4c850594e03c
Oleh Luchkiv <olesho@gmail.com>
2019-03-16 21:22:58 +0800

    added golang version

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
-# tuner #
+# comboss #
 Finds best combination of parameters for any script
 
+## python3 ##
 Usage
 > Consdering you want to find best configuration for test.py:
 ```
-STR_PARAM1=[val1,val2] NUM_PARAM2=[10,45,34,14,564] RANGE_PARAM3=(START=1,STOP=50,STEP=2) tuner.py test.py > output.csv
+STR_PARAM1=[val1,val2] NUM_PARAM2=[10,45,34,14,564] RANGE_PARAM3=(START=1,STOP=50,STEP=2) comboss.py test.py > output.csv
 ```
 All combinations will be tested and results written to CSV:
 ```
@@ -16,5 +17,18 @@ STR_PARAM1=val1 NUM_PARAM2=45 RANGE_PARAM3=1 test.py
 
 Test:
 ```
-ENUM=[wo,jhi,yay] RANGE=(START=1,STOP=10,STEP=2) ./tuner.py 'python3 test.py
+ENUM=[wo,jhi,yay] RANGE=(START=1,STOP=10,STEP=2) ./comboss.py 'python3 test.py
+```
+
+## go ##
+
+Installation
+```
+go get -u github.com/olesho/comboss
+go install github.com/olesho/comboss
+```
+
+Test
+```
+ENUM=[aba,abr,abrvalg] RANGE=(1,5,.2) comboss python3 test.py
 ```
--- main.go
+++ main.go
@@ -1,1 +1,138 @@
-package tuner
+package main
+
+import (
+	"os"
+	"strings"
+	"fmt"
+	"strconv"
+	"os/exec"
+)
+
+func main() {
+	environ := os.Environ()
+
+	for _, pair := range environ {
+		keyVal := strings.Split(pair, "=")
+		if len(keyVal) == 2 {
+			key := keyVal[0]
+			val := keyVal[1]
+			if strings.HasPrefix(keyVal[1], "[") && strings.HasSuffix(keyVal[1], "]") {
+				vals := strings.Split(val[1:len(val)-1], ",")
+				params[key] = vals
+			} else if strings.HasPrefix(val, "(") && strings.HasSuffix(val, ")") {
+				vals := strings.Split(val[1:len(val)-1], ",")
+				if len(vals) == 3 {
+					start := vals[0]
+					stop := vals[1]
+					step := vals[2]
+
+					if len(start) > 6 {
+						if strings.ToLower(start[:6]) == "start=" {
+							start = start[6:]
+						}
+					}
+
+					if len(stop) > 5 {
+						if strings.ToLower(stop[:5]) == "stop=" {
+							stop = stop[5:]
+						}
+					}
+
+					if len(step) > 5 {
+						if strings.ToLower(step[:5]) == "step=" {
+							step = step[5:]
+						}
+					}
+
+					vals := []string{}
+					if startInt, stopInt, stepInt, ok := areInts(start, stop, step); ok {
+						for ; startInt <= stopInt; startInt += stepInt {
+							vals = append(vals, strconv.Itoa(startInt))
+						}
+					}
+
+					if startFloat, stopFloat, stepFloat, ok := areFloats(start, stop, step); ok {
+						for ; startFloat <= stopFloat; startFloat += stepFloat {
+							vals = append(vals, fmt.Sprintf("%f", startFloat))
+						}
+					}
+
+					params[key] = vals
+				}
+			}
+
+		}
+	}
+
+	for k := range params {
+		keys = append(keys, k)
+	}
+
+	next_param(0, make(map[string]string))
+
+
+}
+
+var keys = []string{}
+var combos = []string{}
+var params = make(map[string][]string)
+
+func serialize(combo map[string]string) []string {
+	results := []string{}
+	for k,v := range combo {
+		results = append(results, k + "=" + v)
+	}
+	return results
+}
+
+func next_param(param_index int, current_combo map[string]string) {
+	if param_index == len(params) {
+		// exec
+		cmd := exec.Command(os.Args[1], os.Args[2:]...)
+		cmd.Env = serialize(current_combo)
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		cmd.Start()
+
+		//fmt.Println(, current_combo)
+		return
+	}
+	for _, val := range params[keys[param_index]] {
+		current_combo[keys[param_index]] = val
+
+		current_combo_copy := make(map[string]string)
+		for k,v := range current_combo {
+			current_combo_copy[k] = v
+		}
+
+		next_param(param_index+1, current_combo_copy)
+	}
+}
+
+func areInts(vals ... string) (start int, stop int, step int, succ bool) {
+	var err error
+	if start, err = strconv.Atoi(vals[0]); err != nil {
+		return 0, 0, 0, false
+	}
+	if stop, err = strconv.Atoi(vals[1]); err != nil {
+		return 0, 0, 0, false
+	}
+	if step, err = strconv.Atoi(vals[2]); err != nil {
+		return 0, 0, 0, false
+	}
+	return start, stop, step,true
+}
+
+func areFloats(vals ... string) (start float64, stop float64, step float64, succ bool) {
+	var err error
+	if start, err = strconv.ParseFloat(vals[0], 64); err != nil {
+		return 0, 0, 0, false
+	}
+	if stop, err = strconv.ParseFloat(vals[1], 64); err != nil {
+		return 0, 0, 0, false
+	}
+	if step, err = strconv.ParseFloat(vals[2], 64); err != nil {
+		return 0, 0, 0, false
+	}
+	return start, stop, step,true
+}
